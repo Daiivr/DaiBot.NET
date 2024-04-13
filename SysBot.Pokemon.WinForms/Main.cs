3e5478603e1f5f839ad643e2ca06f7e73b2f145c
Dai <itsdaivr@gmail.com>
2024-04-13 01:50:13 -0400

    Eliminado eco de inicio/parada

--- SysBot.Pokemon.WinForms/Main.cs
+++ SysBot.Pokemon.WinForms/Main.cs
@@ -294,8 +294,6 @@ public sealed partial class Main : Form
     {
         foreach (var c in FLP_Bots.Controls.OfType<BotController>())
             c.SendCommand(cmd, false);
-
-        EchoUtil.Echo($"A todos los robots se les ha emitido un comando para {cmd}.");
     }
 
     private void B_Stop_Click(object sender, EventArgs e)
