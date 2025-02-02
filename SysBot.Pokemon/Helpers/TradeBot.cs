e1ad1a72c46d6a66da4f9badcc4fafbf035c3993
Dai <itsdaivr@gmail.com>
2025-02-01 20:45:44 -0500

    Actualización para las últimas Incursiones/Distribuciones y Regalos Misteriosos
    - KOR 아론's Lucario
    - HOME Manaphy / Enamorus

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v5.0";
+        public const string Version = "v5.1";
     }
 }
